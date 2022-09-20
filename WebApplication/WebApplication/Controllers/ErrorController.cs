7e2de7e8fe07386cb6b3195ac5431b00e9000441
Polkovnik-Daniil <0205danik@mail.ru>
2022-09-20 10:29:55 +0300

    version 0.4

---
+++ WebApplication/WebApplication/Controllers/ErrorController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication.Controllers {
+    public class ErrorController : Controller {
+        // GET: Error
+        public ActionResult Error() {
+            Response.StatusCode = 404;
+            ViewBag.url = Request.RawUrl;
+            ViewBag.method = Request.HttpMethod;
+            return View();
+        }
+    }
+}
